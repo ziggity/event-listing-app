4fb0a20428bd4b561fbb2d766a6236e6416caefb
zach beecher <zachbeecher@gmail.com>
2018-07-23 12:49:37 -0700

    added package.json starter dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app/layout/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById('root');
+
+let render = () => {
+    ReactDOM.render(<App />, rootEl)
+}
+
+if(module.hot) {
+    module.hot.accept('./app/layout/App', () => {
+        setTimeout(render)
+    })
+}
+
+render();
 registerServiceWorker();
