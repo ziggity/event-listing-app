f2cb0f99553d7869937827d85ce311b327de7c4e
zach beecher <zachbeecher@gmail.com>
2018-07-25 15:55:29 -0700

    static website built - next up add redux data flow inversely

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,19 +1,71 @@
-import React, { Component } from 'react';
-import Grid from 'semantic-ui-react/dist/commonjs/collections/Grid/Grid';
+import React, { Component } from "react";
+import { Grid, Button } from "semantic-ui-react";
+import EventList from "../EventList/EventList";
+import EventForm from "../EventForm/EventForm";
 
-class EventDashboard extends Component {
-    render(){
-        return (
-            <Grid>
-            <Grid.Column width={10}>
-                <h2>left col</h2>
-            </Grid.Column>
-            <Grid.Column width={6}>
-                <h2>Right col</h2>
-            </Grid.Column>
-            </Grid>
-                
-        )
+const eventsDashboard = [
+    {
+      id: '1',
+      title: 'Trip to Tower of London',
+      date: '2018-03-27T11:00:00+00:00',
+      category: 'culture',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
+      city: 'London, UK',
+      venue: "Tower of London, St Katharine's & Wapping, London",
+      hostedBy: 'Bob',
+      hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
+      attendees: [
+        {
+          id: 'a',
+          name: 'Bob',
+          photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
+        },
+        {
+          id: 'b',
+          name: 'Tom',
+          photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
+        }
+      ]
+    },
+    {
+      id: '2',
+      title: 'Trip to Punch and Judy Pub',
+      date: '2018-03-28T14:00:00+00:00',
+      category: 'drinks',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
+      city: 'London, UK',
+      venue: 'Punch & Judy, Henrietta Street, London, UK',
+      hostedBy: 'Tom',
+      hostPhotoURL: 'https://randomuser.me/api/portraits/men/22.jpg',
+      attendees: [
+        {
+          id: 'b',
+          name: 'Tom',
+          photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
+        },
+        {
+          id: 'a',
+          name: 'Bob',
+          photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
+        }
+      ]
     }
+  ];
+class EventDashboard extends Component {
+  render() {
+    return (
+      <Grid>
+        <Grid.Column width={10}>
+          <EventList events={eventsDashboard}/>
+        </Grid.Column>
+        <Grid.Column width={6}>
+        <Button positive content="Create Event" />
+        <EventForm />
+        </Grid.Column>
+      </Grid>
+    );
+  }
 }
-export default EventDashboard
+export default EventDashboard;
